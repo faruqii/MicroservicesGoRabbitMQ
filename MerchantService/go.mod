dda043b6808a229a87b528f931c6c30df31ac8ba
faruqii <faruqihafiz21@gmail.com>
2024-05-11 22:24:42 +0700

    update

--- MerchantService/go.mod
+++ MerchantService/go.mod
@@ -2,4 +2,7 @@ module MerchantSVC
 
 go 1.22.3
 
-require github.com/streadway/amqp v1.1.0
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/streadway/amqp v1.1.0
+)
--- MerchantService/main.go
+++ MerchantService/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/streadway/amqp"
+	"github.com/joho/godotenv"
 )
 
 type PurchaseNotification struct {
@@ -15,7 +18,20 @@ type PurchaseNotification struct {
 }
 
 func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	err := godotenv.Load()
+
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
+
+	amqpURL := fmt.Sprintf("amqp://%s:%s@%s:%s/",
+		os.Getenv("RABBITMQ_USER"),
+		os.Getenv("RABBITMQ_PASSWORD"),
+		os.Getenv("RABBITMQ_HOST"),
+		os.Getenv("RABBITMQ_PORT"),
+	)
+	conn, err := amqp.Dial(amqpURL)
 	if err != nil {
 		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
 	}
